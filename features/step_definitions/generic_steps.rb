dbb184c8de76d9b74a169f88ea1ba53379cdc38f
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2014-08-11 22:51:29 +0100

    Test creating and listing notes, listing and associating tags

---
+++ features/step_definitions/generic_steps.rb
@@ -0,0 +1,23 @@
+When(/^I visit the homepage$/) do
+  visit "/"
+end
+
+When(/^I visit Notes$/) do
+	visit "/browse/notes"
+end
+
+When(/^I visit Tags$/) do
+	visit "/tags/all"
+end
+
+Then(/^I should see "(.*?)"$/) do |content|
+  page.should have_content(content)
+end
+
+Then(/^I type in "(.*?)" in "(.*?)"$/) do |text, field|
+	fill_in field, with: text
+end
+
+When(/^I click on "(.*?)"$/) do |label|
+	click_on label
+end
--- features/step_definitions/homepage_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I visit the homepage$/) do
-  visit "/"
-end
-
-Then(/^I should see "(.*?)"$/) do |content|
-  page.should have_content(content)
-end
