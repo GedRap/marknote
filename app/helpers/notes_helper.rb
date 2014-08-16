b078b3784f44b7c53ab0978ba6fe9255fbf76265
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2014-08-16 22:45:11 +0100

    Use view helper to render note widget

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,24 @@
 module NotesHelper
+	def render_note_widget(note)
+		html = "<div class='col-md-4'>
+				<div class='panel panel-default'>
+					<div class='panel-heading'>
+						<h3 class='panel-title'>
+							<a href='/notes/#{note.id}'>#{note.title}</a>
+						</h3>
+					</div>
+					<div class='panel-body'>"
+		if note.tags.length > 0
+			html += "<p>Tagged under "
+			note.tags.each do |tag|
+				html += "<a href='/tag/#{tag.id}/notes'>#{tag.name}</a> "
+			end
+		end
+		
+		html += "<p class='timestamp'>#{note.created_at}, Views: #{note.views}</p>
+					</div>
+				</div>
+			</div>"
+		return html.html_safe
+	end
 end
