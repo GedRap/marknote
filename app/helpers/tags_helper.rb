a38bcd7eabec13d541574b2e8c172355675a2fc1
Gediminas Rapolavicius <gediminas.rap@gmail.com>
2014-08-17 23:22:03 +0100

    View helper for notes widget

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,22 @@
 module TagsHelper
+	def render_tag_widget(tag)
+		html = "<div class='col-md-4'>
+				<div class='panel panel-default'>
+					<div class='panel-heading'>
+						<h3 class='panel-title'>
+							<a href='/tag/#{tag.id}/notes'>#{tag.name}</a>
+						</h3>
+					</div>
+					<div class='panel-body'><p>Some of the notes:</p>"
+					
+		tag.notes[0..2].each do |note|
+			html += "<p><a href='/notes/#{note.id}'>#{note.title}</a></p>"
+		end
+		
+		html += "<p class='timestamp'>Total Notes: #{tag.notes.length}</p>
+					</div>
+				</div>
+			</div>"
+		return html.html_safe
+	end
 end
